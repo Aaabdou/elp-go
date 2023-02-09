4265d23de2d5f3c0e6b01465790b9d908f986e9c
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-02-09 13:38:43 +0100

    version 3.1

---
+++ ProduitMatriciel/Phase3/client-1.go
@@ -0,0 +1,305 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Couldn't connect to server. Error :")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Tcp connection established on port", CONN_PORT)
+	for {
+		fmt.Println("1. Send problem from file to server")
+		fmt.Println("2. Send problem from generated matrix to server")
+		fmt.Println("3. End connection")
+		fmt.Println()
+		var answer string
+		fmt.Scanln(&answer)
+		if answer == "1" {
+			s, err := client(conn, true)
+			if s == "Error connection" {
+				fmt.Println("Error encountered :", err)
+				break
+			} else if err != nil {
+				fmt.Println("Error encountered :", err)
+			}
+			if s == "STOP" {
+				fmt.Println("TCP client exiting...")
+				wr := bufio.NewWriter(conn)
+				wr.WriteString("STOP:")
+				conn.Close()
+				break
+			}
+
+		} else if answer == "2" {
+			s, err := client(conn, false)
+			if s == "Error connection" {
+				fmt.Println("Error encountered :", err)
+				break
+			} else if err != nil {
+				fmt.Println("Error encountered :", err)
+			}
+			if s == "STOP" {
+				fmt.Println("TCP client exiting...")
+				wr := bufio.NewWriter(conn)
+				wr.WriteString("STOP:")
+				conn.Close()
+				break
+			}
+
+		} else if answer == "3" {
+			wr := bufio.NewWriter(conn)
+			wr.WriteString("STOP:")
+			conn.Close()
+			break
+		} else {
+			fmt.Println("Couldn't understand your answer. Please choose between")
+		}
+
+	}
+
+}
+func client(conn net.Conn, file bool) (string, error) {
+	fmt.Println("You can end connection anytime by typing STOP")
+	var result [][]int
+	var matA string
+	var matB string
+
+	//Demander le fichier des matrices
+	if file {
+		fmt.Print("Adress matrixA:")
+		fmt.Scanln(&matA)
+		if matA == "STOP" {
+			return "STOP", nil
+		}
+
+		fmt.Print("Adress matrixB:")
+		fmt.Scanln(&matB)
+		if matB == "STOP" {
+			return "STOP", nil
+		}
+
+		//Envoi des matrices au serveur
+		err := sendFile("matrixA", matA, conn)
+		if err != nil {
+			return "Error", err
+		}
+		err = sendFile("matrixB", matB, conn)
+		if err != nil {
+			return "Error", err
+		}
+	} else {
+		var rowA int
+		var columnA int
+		var columnB int
+		var err error
+		for true {
+
+			fmt.Println("Rows matrixA:")
+			fmt.Scanln(&matA)
+			if matA == "STOP" {
+				return "STOP", nil
+			}
+			if matA == "random" {
+				rowA = 0
+				break
+			} else {
+				rowA, err = strconv.Atoi(matA)
+				fmt.Println("mat", matA, "row", rowA, "err", err)
+				if err == nil {
+					break
+				}
+			}
+		}
+		for true {
+			fmt.Println("Columns matrixA:")
+			fmt.Scanln(&matA)
+			if matA == "STOP" {
+				return "STOP", nil
+			}
+			if matA == "random" {
+				columnA = 0
+				break
+			} else {
+				columnA, err = strconv.Atoi(matA)
+				fmt.Println("mat", matA, "column", columnA, "err", err)
+				if err == nil {
+					break
+				}
+			}
+		}
+
+		matrixA, _, columnA := matrixGenerator(rowA, columnA)
+		fmt.Println(matrixA)
+		for true {
+			var err error
+			fmt.Print("Columns matrixB:")
+			fmt.Scanln(&matB)
+
+			if matB == "STOP" {
+				return "STOP", nil
+			}
+			if matB == "random" {
+				columnB = 0
+				break
+			} else {
+				columnB, err = strconv.Atoi(matB)
+				if err == nil {
+					break
+				}
+			}
+
+		}
+		matrixB, _, _ := matrixGenerator(columnA, columnB)
+		fmt.Println(matrixB)
+		//Envoi des matrices au serveur
+		err = sendStringMatrix("matrixA", matrixA, conn)
+		if err != nil {
+			return "Error", err
+		}
+		err = sendStringMatrix("matrixB", matrixB, conn)
+		if err != nil {
+			return "Error", err
+		}
+	}
+
+	//Réception de la réponse du serveur
+	received := false
+	for !received {
+		rd := bufio.NewReader(conn)
+		ba, err := rd.ReadString(byte(':'))
+		ba = strings.Trim(ba, ":")
+		if err != nil {
+			return "Error connection", err
+		}
+
+		//Vérifie que c'est la réponse du erveur
+		if ba == "matrixC" {
+			fmt.Println(ba)
+			received = true
+			//Reçoit la taille de la matrice finale
+			ligne, _, err := rd.ReadRune()
+			if err != nil {
+				return "Error connection", err
+			}
+			col, _, err := rd.ReadRune()
+			if err != nil {
+				return "Error connection", err
+			}
+			fmt.Println(ligne, ":", col)
+			//Réception matrice finale
+			result = make([][]int, ligne)
+			for i := 0; i < int(ligne); i++ {
+				for j := 0; j < int(col); j++ {
+					valS, err := rd.ReadString(':')
+					if err != nil {
+						return "Error connection", err
+					}
+					valT := strings.Trim(valS, ":")
+					val, err := strconv.Atoi(valT)
+					if err != nil {
+						return "Str conversion not possible", err
+					}
+
+					result[i] = append(result[i], val)
+				}
+			}
+			fmt.Println()
+		}
+
+	}
+	//Affichage réponse
+	for _, row := range result {
+		for _, val := range row {
+			fmt.Print(val, " ")
+		}
+		fmt.Println()
+	}
+	return "", nil
+
+}
+func sendFile(name string, f string, conn net.Conn) error {
+	//Ouvre fichier
+	file, err := os.Open(f)
+	if err != nil {
+		return err
+	}
+
+	fileInfo, err := file.Stat()
+	fmt.Println("Start sending file ", f, "!")
+	wr := bufio.NewWriter(conn)
+	wr.WriteString(name + ":")
+	fileSize := strconv.FormatInt(fileInfo.Size(), 10)
+	wr.WriteString(fileSize + ":")
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		wr.WriteString(scanner.Text() + "\n")
+
+	}
+	wr.WriteString("EOF\n")
+
+	wr.Flush()
+	fmt.Println("File ", f, " has been sent!")
+
+	return nil
+
+}
+func sendStringMatrix(name string, mat string, conn net.Conn) error {
+
+	fmt.Println("Start sending", name, "!")
+	wr := bufio.NewWriter(conn)
+	wr.WriteString(name + ":")
+	fileSize := strconv.Itoa(len(mat) * 4)
+	wr.WriteString(fileSize + ":")
+
+	scanner := bufio.NewScanner(strings.NewReader(mat))
+	for scanner.Scan() {
+		wr.WriteString(scanner.Text() + "\n")
+		fmt.Println(scanner.Text())
+	}
+	wr.WriteString("EOF\n")
+
+	wr.Flush()
+	fmt.Println(name, " has been sent!")
+
+	return nil
+}
+func matrixGenerator(row int, column int) (string, int, int) {
+	var matrix string = ""
+	fmt.Println("row", row, "column", column)
+	if row <= 0 {
+		row = rand.Intn(100)
+		fmt.Println("changed")
+	}
+	if column <= 0 {
+		column = rand.Intn(100)
+		fmt.Println("changed")
+	}
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < column-1; j++ {
+			matrix += strconv.Itoa(rand.Intn(1000)) + " "
+		}
+		matrix += strconv.Itoa(rand.Intn(1000)) + "\n"
+		//fmt.Println("aa", i, matrix)
+	}
+	fmt.Println("row", row, "column", column)
+	return matrix, row, column
+}
---
+++ ProduitMatriciel/Phase3/serverV3-1.go
@@ -0,0 +1,262 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+const (
+	SERV_HOST = "localhost"
+	SERV_PORT = "3333"
+	SERV_TYPE = "tcp"
+)
+
+type line struct {
+	nb   int
+	line []int
+	err  error
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	chConn := make(chan net.Conn)
+	running := true
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go worker(chConn, wg)
+	}
+	fmt.Println("Server starting...")
+	l, err := net.Listen(SERV_TYPE, SERV_HOST+":"+SERV_PORT)
+	defer l.Close()
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	// Listen for an incoming connection.
+	go user(&running)
+	for running {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		chConn <- conn
+		fmt.Println("bb")
+
+	}
+	fmt.Println("aa")
+	close(chConn)
+}
+func user(run *bool) {
+	fmt.Println("If you want to close the server, type 1")
+	var answer string
+	for true {
+		fmt.Scanln(&answer)
+		if answer == "1" {
+			*run = false
+			fmt.Println("exiting..")
+			break
+		} else {
+			fmt.Println("Didn't understand your request")
+			fmt.Println("If you want to close the server, type 1")
+		}
+	}
+
+}
+func worker(can chan net.Conn, wg *sync.WaitGroup) {
+	client_exit := false
+	for conn := range can {
+		fmt.Println("New client")
+		for !client_exit {
+
+			var Aexist = false
+			var Bexist = false
+			var matrixA [][]int
+			var matrixB [][]int
+
+			for !Aexist || !Bexist {
+				rd := bufio.NewReader(conn)
+				ba, err := rd.ReadString(byte(':'))
+				name := strings.Trim(ba, ":")
+				fmt.Println(name)
+				if err != nil {
+					fmt.Println("Error encountered", err)
+					client_exit = true
+					break
+				}
+
+				if name == "matrixA" {
+					ba, err = rd.ReadString(byte(':'))
+					if err != nil {
+						fmt.Println("Error encountered", err)
+						client_exit = true
+						break
+					}
+					ba = strings.Trim(ba, ":")
+					fileSize, _ := strconv.ParseInt(ba, 10, 64)
+					fmt.Println(name, " size :", fileSize)
+					matrixA, err = readMatrixFromClient(rd)
+					if err != nil {
+
+						fmt.Println("Error encountered", err)
+						continue
+					}
+					Aexist = true
+
+				} else if name == "matrixB" {
+					ba, err = rd.ReadString(byte(':'))
+					if err != nil {
+						fmt.Println("Error encountered", err)
+						client_exit = true
+						break
+					}
+					ba = strings.Trim(ba, ":")
+					fileSize, _ := strconv.ParseInt(ba, 10, 64)
+					fmt.Println(name, " size :", fileSize)
+					matrixB, err = readMatrixFromClient(rd)
+					if err != nil {
+						fmt.Println("Error encountered", err)
+						continue
+					}
+					Bexist = true
+
+				}
+			}
+			if !client_exit {
+				/*fmt.Println(matrixA)
+				fmt.Println(matrixB)*/
+				fmt.Println("Received request from client")
+				result, err := matrixMultiply(matrixA, matrixB)
+				if err != nil {
+					fmt.Println("Error encountered", err)
+					return
+				}
+				sendMatrix("matrixC", result, conn)
+			}
+		}
+	}
+}
+
+func readMatrixFromClient(file io.Reader) ([][]int, error) {
+	scanner := bufio.NewScanner(file)
+	var mLength = 0
+	can := make(chan line)
+
+	for scanner.Scan() {
+		//fmt.Println("test", mLength)
+		if scanner.Text() == "EOF" {
+			break
+		}
+		rowStrings := strings.Fields(scanner.Text()) // Sépare les strings en slice sans espace
+
+		go readLine(can, rowStrings, mLength)
+		mLength++
+
+	}
+	b := scanner.Err()
+	if b != nil {
+		fmt.Println(b)
+	}
+
+	var matrix [][]int = make([][]int, mLength)
+	for j := 0; j < mLength; j++ {
+		//fmt.Println("test", mLength)
+		mLine := <-can
+		if mLine.err != nil {
+			return matrix, mLine.err
+		}
+		matrix[mLine.nb] = mLine.line
+	}
+
+	//fmt.Println(matrix)
+
+	return matrix, nil
+}
+func readLine(c chan line, rowStrings []string, i int) {
+	var row []int
+	l := line{nb: i}
+	l.err = nil
+	for _, valueString := range rowStrings { // range iterates over the slice elements
+		value, err := strconv.Atoi(valueString) // convert string to int
+		if err != nil {
+			l.err = err
+			fmt.Println(err)
+		}
+		row = append(row, value) // apprend new elements to the slice
+	}
+	//fmt.Println("row ", i, " ", row)
+	l.line = row
+	fmt.Println(l.nb, " : ", l.line, ":", l.err)
+	c <- l
+
+}
+func matrixMultiply(matrixA, matrixB [][]int) ([][]int, error) {
+	if len(matrixA[0]) != len(matrixB) {
+		return nil, fmt.Errorf("Number of columns in matrix A does not match number of rows in matrix B")
+	}
+
+	result := make([][]int, len(matrixA)) // create a 2D slice with the size = the length of matrix A
+
+	can := make(chan line)
+	for i := 0; i < len(matrixA); i++ {
+		go lineXMatrix(matrixA[i], matrixB, can, i)
+	}
+	for j := 0; j < len(matrixA); j++ {
+		mLine := <-can
+		result[mLine.nb] = mLine.line
+	}
+
+	//fmt.Println(result)
+	return result, nil
+}
+func lineXMatrix(lin []int, matrix [][]int, c chan line, index int) {
+	var result = make([]int, len(matrix[0]))
+	l := line{nb: index}
+	for j := 0; j < len(matrix[0]); j++ {
+		for k := 0; k < len(lin); k++ {
+			result[j] += lin[k] * matrix[k][j]
+		}
+	}
+	l.line = result
+	c <- l
+}
+func sendMatrix(name string, m [][]int, conn net.Conn) error {
+
+	//Envoi le nom
+	fmt.Println(name)
+
+	wr := bufio.NewWriter(conn)
+	wr.WriteString(name + ":")
+	fmt.Println(len(m), ":", len(m[0]))
+	wr.WriteRune(int32(len(m)))
+	//wr.WriteString(":")
+	wr.WriteRune(int32(len(m[0])))
+
+	wr.Flush()
+
+	for _, row := range m {
+		for _, value := range row {
+			//fmt.Printf("%d ", int32(value))
+			_, err := wr.WriteString(strconv.Itoa(value) + ":")
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+		}
+
+		fmt.Println()
+	}
+	wr.WriteString("EOF\n")
+
+	wr.Flush()
+
+	return nil
+
+}
